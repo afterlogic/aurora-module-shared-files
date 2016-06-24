37d88e851c6ac3728d180b86c753217555ae8051
sash04ek <a.ovcharov@gmail.com>
2016-06-24 09:32:00 +0300

    first commit

---
+++ module.php
@@ -0,0 +1,21 @@
+<?php
+
+class SharedFilesModule extends AApiModule
+{
+	public function init() 
+	{
+		$this->subscribeEvent('Files::GetStorages::after', array($this, 'GetStorages'));
+	}
+	
+	public function GetStorages(&$aResult)
+	{
+		$iUserId = \CApi::getLogginedUserId();
+
+		$aResult['@Result'][] = [
+			'Type' => 'shared', 
+			'IsExternal' => false,
+			'DisplayName' => $this->i18N('LABEL_SHARED_STORAGE', $iUserId)
+		];
+	}
+
+}
